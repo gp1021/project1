c669b3c7916052d779d989f293382a5e82c01e12
gp1021 <60611487+gp1021@users.noreply.github.com>
2020-05-19 16:14:34 -0400

    Update Card.java

--- Higher Or Lower/src/HorL/Card.java
+++ Higher Or Lower/src/HorL/Card.java
@@ -63,31 +63,4 @@ public class Card {
 	public int pointValue() {
 		return pointValue;
 	}
-
-	/** Compare this card with the argument.
-	 * @param otherCard the other card to compare to this
-	 * @return true if the rank, suit, and point value of this card
-	 *              are equal to those of the argument;
-	 *         false otherwise.
-	 */
-	public boolean matches(Card otherCard) {
-		return otherCard.suit().equals(this.suit())
-			&& otherCard.rank().equals(this.rank())
-			&& otherCard.pointValue() == this.pointValue();
-	}
-
-	/**
-	 * Converts the rank, suit, and point value into a string in the format
-	 *     "[Rank] of [Suit] (point value = [PointValue])".
-	 * This provides a useful way of printing the contents
-	 * of a <code>Deck</code> in an easily readable format or performing
-	 * other similar functions.
-	 *
-	 * @return a <code>String</code> containing the rank, suit,
-	 *         and point value of the card.
-	 */
-	@Override
-	public String toString() {
-		return rank + " of " + suit + " (point value = " + pointValue + ")";
-	}
 }
